5a1904954001e4801cfbba65b4e66b6488309c2f
Sugut <sugutvictorine@gmail.com>
2022-09-20 22:39:00 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,16 @@
 import React from "react";
 import blogData from "../data/blog";
 
-
-function About(props){
-  if(props.image){
-  return (
-    <aside>
-        <img src = {props.image} alt="blog logo"/>
-         <p>{props.about}</p>
-    </aside>
-  )
-  }else{
-   return(
-    <aside>
-        <img src ="https://via.placeholder.com/215" alt="blog logo"/>
-         <p>{props.about}</p>
-    </aside>
-   )
-
+const About = (props) => {
+  About.defaultProps = {
+      image:"https://via.placeholder.com/215"
   }
-
+return (
+    <aside>
+        <img src={props.image} alt="blog logo" />
+        <p>{props.about}</p>
+  </aside>
+)
 }
 
 
